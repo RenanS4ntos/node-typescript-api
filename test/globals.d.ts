2db631d5477ea5d9236baa1cd1e635b669fc2502
Renan Santos <renan-bs@hotmail.com>
2023-01-26 09:45:32 -0300

    Setup OvernightJS and Express | First Controller

---
+++ test/globals.d.ts
@@ -0,0 +1,8 @@
+// adicionando tipos globais
+
+declare global {
+  //eslint-disable-next-line no-var
+  var testRequest: import("supertest").SuperTest<import("supertest").Test>;
+}
+
+export {};
---
+++ test/jest-setup.ts
@@ -0,0 +1,9 @@
+// inicializar o servidor para todos os testes funcionar 
+import { SetupServer } from '@src/server';
+import supertest from 'supertest';
+
+beforeAll(() => {
+  const server = new SetupServer();
+  server.init();
+  global.testRequest = supertest(server.getApp());
+})
